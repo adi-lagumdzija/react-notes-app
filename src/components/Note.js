820ad1bc8201ea276d30ac2771bd11eac460dacf
adi-lagumdzija <adilag0511@gmail.com>
2022-10-23 20:17:45 +0200

    adding delete option

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,14 @@
 import { MdDeleteForever } from "react-icons/md"
-const Note = ({id, text, date}) => {
+const Note = ({id, text, date, handleDeleteNote}) => {
     return(
         <div className="note">
             <span>{text}</span>
             <div className="noteFooter">
                 <small>{date}</small>
-                <MdDeleteForever className="deleteIcon" size='1.3em'></MdDeleteForever>
+                <MdDeleteForever onClick={()=> handleDeleteNote(id)} className="deleteIcon" size='1.3em' ></MdDeleteForever>
             </div>
         </div>
     )
 }
 
-export default Note;
+export default Note; 
